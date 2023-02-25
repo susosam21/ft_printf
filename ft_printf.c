7ba6d823b33c4475d3d59d27e65dfe3979167fe4
Sarah Mohammed <samohamm@lab1r2s9.42abudhabi.ae>
2023-02-25 22:43:41 +0400

    clean

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: samohamm <samohamm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 13:59:56 by samohamm          #+#    #+#             */
-/*   Updated: 2023/02/25 15:23:18 by samohamm         ###   ########.fr       */
+/*   Updated: 2023/02/25 22:41:37 by samohamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,43 +38,3 @@ int	ft_printf(const char *s, ...)
 	va_end(args);
 	return (len);
 }
-
-// int G = 0;
-// int main()
-// {
-// 	char 				c 	= 	'S';
-// 	char 				*s	= 	NULL;
-// 	int 				i 	= 	-2147483648;
-// 	int 				u 	= 	19;
-// 	unsigned	char 	a	=	0x64;
-// 	int 				b	=	0xFAFA;
-// 	int					ret;
-// 	int					reter;
-// 	static int 			o;
-//     int 				y;
-//     int 				*p;
-
-// 	p = malloc(sizeof(int));
-// 	ret = printf("HELLO NEW PROJECT This Char = %c AND This String 
-//= %s AND This Intger %i AND This Digit %d AND This Unsigned %u \n AND 
-//This HEX %X AND This hex %x \n", c, s, i, i, u, a, b);
-// 	printf("printf returns %d bytes\n", ret);
-// 	reter = ft_printf("HELLO NEW PROJECT This Char =
-// %c AND This String = %s AND This Intger %i AND This Digit 
-//%d AND This Unsigned %u 
-//\n AND This HEX %X AND This hex %x \n", c, s, i, i, u, a, b);
-// 	printf("ft_printf returns %d bytes\n", reter);
-// 	printf("*******************************************************\n");
-// 	printf("&G   = %p\n", (void *) &G);
-// 	printf("&o   = %p\n", (void *) &s);
-// 	printf("&y   = %p\n", (void *) &a);
-// 	printf("&p   = %p\n", (void *) &p);
-// 	printf("p    = %p\n", (void *) p);
-// 	printf("<<<<<<<<<<<<<<<**************************>>>>>>>>>>>>>>>\n");
-// 	ft_printf("&G   = %p\n", (void *) &G);
-// 	ft_printf("&o   = %p\n", (void *) &s);
-// 	ft_printf("&y   = %p\n", (void *) &a);
-// 	ft_printf("&p   = %p\n", (void *) &p);
-// 	ft_printf("p    = %p\n", (void *) p);
-// 	free(p);
-// }
